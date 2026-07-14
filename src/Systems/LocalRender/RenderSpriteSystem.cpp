bd6645302caab59bb125d89c0cf854575e7089f5
artemenko_pa <artemenko_pa@magnit.ru>
2026-07-14 10:09:21 +0300

    remove debug from sprite renderer

--- src/Systems/LocalRender/RenderSpriteSystem.cpp
+++ src/Systems/LocalRender/RenderSpriteSystem.cpp
@@ -9,21 +9,18 @@
 #include "Components/Impl/Sprite.h"
 
 
-void RenderSpriteSystem(const flecs::world &world) {
+void RenderSpriteSystem(const flecs::world& world) {
     world
-            .system<const Position, const Sprite, const Rotation>(__func__)
-            .kind(flecs::OnStore)
-            .each([](const Position &position, const Sprite &sprite, const Rotation &rotation) {
-                const auto dstRect = sprite.GetDstRect({toFloat(position.x), toFloat(position.y)});
-                DrawTexturePro(
-                    *sprite.texture,
-                    sprite.GetSrcRect(),
-                    sprite.GetDstRect({toFloat(position.x), toFloat(position.y)}),
-                    sprite.GetCenterOrigin(),
-                    rotation.value * RAD2DEG + sprite.rotation * RAD2DEG,
-                    WHITE
-                );
-
-                DrawCircle(toInt(dstRect.x), toInt(dstRect.y), 10.0f, WHITE);
-            });
+        .system<const Position, const Sprite, const Rotation>(__func__)
+        .kind(flecs::OnStore)
+        .each([](const Position& position, const Sprite& sprite, const Rotation& rotation) {
+            DrawTexturePro(
+                *sprite.texture,
+                sprite.GetSrcRect(),
+                sprite.GetDstRect({toFloat(position.x), toFloat(position.y)}),
+                sprite.GetCenterOrigin(),
+                rotation.value * RAD2DEG + sprite.rotation * RAD2DEG,
+                WHITE
+            );
+        });
 }
